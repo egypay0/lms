7ffdce2b7af083ffbc05b3c07f6b095bac13d371
egypay0 <info@egypay.com>
2024-01-04 23:06:11 +0200

    New Update

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-           <h1>Label Management Software</h1>
+           <h1 >Label </h1>
           
       </div>
  
