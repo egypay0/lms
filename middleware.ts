ddf295c767eebc4571b9d7096cf1a5abad291784
egypay0 <info@egypay.com>
2024-01-06 23:28:52 +0200

    new update 8

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,14 @@
- 
- export { default } from 'next-auth/middleware'
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
- export const config ={matcher: ['/dashboard']};
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+ }
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+
