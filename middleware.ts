ce148d3206e11f622f90ee933520d906ee108fe0
egypay0 <info@egypay.com>
2024-01-06 23:33:30 +0200

    new update 8

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@
 
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+export { default } from 'next-auth/middleware'
 
 export function middleware(request: NextRequest) {
  }
