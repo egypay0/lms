cc1e03d3a92722a78c30d3786d629e5ce029761d
egypay0 <info@egypay.com>
2024-01-09 01:57:01 +0200

    v0.3

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,14 +1,12 @@
 
 import Form from "./form"
-import {getServerSession} from 'next-auth'
-import { redirect } from "next/navigation";
+ import { redirect } from "next/navigation";
 
 export default async function RegisterPage(){
  
-    const session = await getServerSession();
-    if (session){
+ 
         redirect("/")
-    }
+     
     
 
 
