2d8a2fe42e0b22aa68ad322748c6487682107234
egypay0 <info@egypay.com>
2024-01-09 02:14:44 +0200

    v0.3

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -5,7 +5,7 @@ const connection = {};
 export const connectToDB = async () => {
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect("mongodb+srv://mt:123654@cluster0.exqrwzj.mongodb.net/dashboard?retryWrites=true&w=majority");
+    const db = await mongoose.connect(process.env.MONGO);
     connection.isConnected = db.connections[0].readyState;
     console.log(connection.isConnected ,"dd")
   } catch (error) {
