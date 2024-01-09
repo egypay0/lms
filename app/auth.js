2faaea2cb4c7e37e3583b808df6fe7c0c52c10cb
egypay0 <info@egypay.com>
2024-01-09 02:52:27 +0200

    v0.3

--- app/auth.js
+++ app/auth.js
@@ -41,8 +41,8 @@ export const { signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
-  secret: "0328f6acdf6e86c8f6cc3576a2bbfc7696756efabdd3c960719a262c0045c2e4",
-  // ADD ADDITIONAL INFORMATION TO SESSION
+  
+   // ADD ADDITIONAL INFORMATION TO SESSION
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
