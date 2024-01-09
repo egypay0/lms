086d262d2e618b5491ceabb94180f603c7a8fa32
egypay0 <info@egypay.com>
2024-01-09 02:13:37 +0200

    v0.3

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
  import Image from 'next/image'
 import { redirect } from 'next/navigation'
  export default function Home() {
+  redirect("/dashboard");
   return (
     <main className=" ">
       <div className=" ">
-         <h1 >Hello</h1>
-           
+            
       </div>
  
        
     </main>
   )
-  redirect("/dashboard");
+   
 }
